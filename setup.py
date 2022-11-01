2c770efa92c2f5049fa0d33a0c517d68af0db4c7
Alexander Mayorov <alexander.majorov@gmail.com>
2022-11-01 15:54:40 +0100

    Up requirements

--- setup.py
+++ setup.py
@@ -44,7 +44,8 @@ setup(
     long_description=get_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/frontdevops/nosql-storage-wrapper",
-    download_url="https://github.com/frontdevops/nosql-storage-wrapper/archive/refs/tags/0.1.1.tar.gz",
+    download_url=("https://github.com/frontdevops/nosql-storage-wrapper/archive/refs/tags/"
+                  f'{project_meta["project"]["version"]}.tar.gz'),
     project_urls={
         "Documentation": "https://github.com/frontdevops/nosql-storage-wrapper/blob/main/README.md",
         "Source": "https://github.com/frontdevops/nosql-storage-wrapper",
@@ -68,7 +69,7 @@ setup(
     install_requires=[
         "motor==3.1.1",
         "pymongo==4.3.2",
-        "magic-config==0.1.10",
+        "magic-config>=0.1.10",
     ],
     extras_require={
         "dev": [
